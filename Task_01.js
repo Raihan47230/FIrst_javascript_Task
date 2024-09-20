8bd61b14e5c3723f4b00e12c20ec83c8b1a88c8b
Raihan <korimon633@gmail.com>
2024-09-20 12:51:36 +0600

    first commit

---
+++ Task_01.js
@@ -0,0 +1,18 @@
+/* Task - 01
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 50; 
+let cokePrice;
+
+if (burgerPrice > 500) {
+    cokePrice = 0; 
+    console.log("Congratulations! You get a free Coke.");
+} else {
+    cokePrice = 30; 
+    console.log("Coke will cost 30tk.");
+}
+
+console.log(`The price of the Coke is:`, cokePrice);
